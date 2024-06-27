5bf85c61dbbbe18bf0001b3b5a26d7cef9939384
starlitWixen <starlitwixen@gmail.com>
2024-06-27 15:29:26 -0400

    Initial commit

---
+++ src/main/java/com/starlitwixen/dndoriginsexpanded/DNDOriginsExpandedClient.java
@@ -0,0 +1,10 @@
+package com.starlitwixen.dndoriginsexpanded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DNDOriginsExpandedClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/starlitwixen/dndoriginsexpanded/DNDOriginsExpandedDataGenerator.java
@@ -0,0 +1,11 @@
+package com.starlitwixen.dndoriginsexpanded;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DNDOriginsExpandedDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
